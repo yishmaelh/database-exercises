b01fb3a2fb2a101e4424c86517321cc89b4f6c28
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-23 14:57:27 -0500

    nevermind

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,7 @@
 use codeup_test_db;
 
-select name as 'Pink Floyd albums' from albums where artist = 'Pink Floyd'
+select name as 'Pink Floyd albums' from albums where artist = 'Pink Floyd';
+
+select release_date as 'Lonely Hearts Club band was released in:' from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+select genre as 'Nevermind is considered:' from albums where  name = 'Nevermind';
