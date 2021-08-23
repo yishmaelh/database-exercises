9ef7fdfbce7a739bc3f64189f4cda8e89e0bf1b9
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-23 15:06:20 -0500

    continuing on selectors

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,3 +5,7 @@ select name as 'Pink Floyd albums' from albums where artist = 'Pink Floyd';
 select release_date as 'Lonely Hearts Club band was released in:' from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 select genre as 'Nevermind is considered:' from albums where  name = 'Nevermind';
+
+select name as 'These albums released in the 1990s:' from albums where release_date between 1990 and 1999;
+
+select name as 'These albums sold under 20 million copies' from albums where sales < 20.0;
