d7258c6eb1cf319d9c30a64912afc58d3d92fee8
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-24 11:35:47 -0500

    part two complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,4 +4,14 @@ select * from employees where first_name in ('Irena', 'Vidya', 'Maya');
 
 select * from employees where last_name like 'e%';
 
-select from employees where last_name like '%q%';
+select * from employees where last_name like '%q%';
+
+select * from employees where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+select gender, COUNT(gender) from employees where gender = 'M' and (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya');
+
+select * from employees where last_name like 'e%' or last_name like '%e';
+
+select * from employees where last_name like 'e%' and last_name like '%e%';
+
+select * from employees where last_name like '%q%' and last_name and '%qu%';
