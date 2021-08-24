814c0f1e732b4a617d3e2cf75d7f49e9b447581a
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-24 11:14:54 -0500

    part one complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,1 +1,7 @@
 use employees;
+
+select * from employees where first_name in ('Irena', 'Vidya', 'Maya');
+
+select * from employees where last_name like 'e%';
+
+select from employees where last_name like '%q%';
