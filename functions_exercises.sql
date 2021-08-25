e06fba71fbb3fcb458564c10c57ceba94e7eb61f
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-25 09:26:55 -0500

    exercise complete

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -9,3 +9,21 @@ SELECT *
 FROM employees.employees
 WHERE month(birth_date) = 12
   AND day(birth_date) = 25;
+
+SELECT *
+FROM employees.employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND month(birth_date) = 12
+  AND day(birth_date) = 25;
+
+SELECT *
+FROM employees.employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND month(birth_date) = 12
+  AND day(birth_date) = 25;
+
+SELECT CONCAT(datediff(curdate(), hire_date)) AS 'Days worked for employees born in the 90''s starting with the oldest'
+FROM employees.employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND month(birth_date) = 12
+  AND day(birth_date) = 25;
