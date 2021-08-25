1016fdf588b19f52c96ad1350e246d2ed9b9c5a3
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-25 09:11:18 -0500

    part two:

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,6 +1,11 @@
 use employees;
 
 SELECT CONCAT(first_name, ' ', last_name, '!')
-FROM employees
+FROM employees.employees
 WHERE last_name LIKE 'e%'
    OR last_name LIKE '%e';
+
+SELECT *
+FROM employees.employees
+WHERE month(birth_date) = 12
+  AND day(birth_date) = 25;
