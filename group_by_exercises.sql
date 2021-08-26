527bab0c83849e715de50bb6ca9edca7b39c325a
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-26 11:47:48 -0500

    updated last problem

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,15 +1,15 @@
 use employees;
 
-SELECT e.last_name
+select e.last_name
 from employees as e
 where e.last_name like 'E%'
- and e.last_name like '%e'
+  and e.last_name like '%e'
 group by e.last_name;
 
 select e.last_name
 from employees as e
 where e.last_name like 'E%'
- and e.last_name like'%e'
+  and e.last_name like '%e'
 group by e.last_name, e.first_name;
 
 
@@ -19,3 +19,13 @@ where e.last_name like '%q%'
   and e.last_name not like '%qu%'
 group by e.last_name;
 
+select e.last_name, COUNT(e.last_name)
+from employees as e
+where e.last_name like '%q%'
+  and e.last_name not like '$qu'
+group by e.last_name;
+
+select e.gender, COUNT(e.gender)
+from employees as e
+where e.first_name in ('Irena', 'Vidya', 'Maya')
+group by e.gender
