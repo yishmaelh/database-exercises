4b10c72509b0dddf25014f4bfe8aeb68026b2b81
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-26 16:00:59 -0500

    part one done

---
+++ join_exercises.sql
@@ -0,0 +1,9 @@
+use employees;
+
+select d.dept_name as 'Department Name', CONCAT(e.first_name, ' ', e.last_name) as 'Department Manager'
+from employees as e
+         join dept_manager as dm on dm.emp_no = e.emp_no
+         join departments as d
+on d.dept_no = dm.dept_no
+where dm.to_date = '9999-01-01'
+order by d.dept_name;
