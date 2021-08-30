deea964971feae33e475dfe238fd60c1f0ebd1d2
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-30 09:11:01 -0500

    exercise complete

--- join_exercises.sql
+++ join_exercises.sql
@@ -17,3 +17,28 @@ from employees as e
 where dm.to_date = '9999-01-01'
   and gender = 'F'
 order by d.dept_name asc;
+
+select distinct title, COUNT(title) as 'Total'
+from employees as e
+         join dept_emp as de
+              on de.emp_no = e.emp_no
+         join departments as d
+              on d.dept_no = de.dept_no
+         join titles as t
+              ON t.emp_no = e.emp_no
+where de.to_date = '9999-01-01' and d.dept_name = 'Customer Service' group by title;
+
+
+
+select d.dept_name as 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) as 'Department Manager',
+       s.salary as Salary
+from employees as e
+         join salaries as s
+              on s.emp_no = e.emp_no
+         join dept_manager as dm
+            on dm.emp_no = s.emp_no
+         join departments as d
+              on d.dept_no = dm.dept_no
+where s.to_date = '9999-01-01' and dm.to_date = '9999-01-01'
+order by `Department Name` asc;
